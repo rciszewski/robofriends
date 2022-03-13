c7fa2467284fadf9db160783997eb9f4ff46e4ce
Ron <rc72095@icloud.com>
2022-03-13 14:37:54 -0400

    added state control of robots for filtering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBar from './SearchBar';
 
 
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>Robofriends</h1>
-      <SearchBar />
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+    })
+
+    return (
+      <div className='tc'>
+        <h1>Robofriends</h1>
+        <SearchBar searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
+ 
 
 export default App;
