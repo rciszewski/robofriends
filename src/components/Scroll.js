56b4f4aface0871e46e364a5f3484c6ce06d8f60
Ron <rc72095@icloud.com>
2023-05-27 21:23:34 -0400

    destructuring state variables

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
   return (
-    <div style={{overflowY: 'scroll', border: '3px solid black', height: '600px'}}>
+    <div style={{overflowY: 'scroll', border: '3px solid black', height: '800px'}}>
       {props.children}
     </div>
   );
