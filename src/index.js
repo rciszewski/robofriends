f3321e43b316d1a5bc3d4b9a74c8b0d32a4bf30c
Ron <rc72095@icloud.com>
2023-07-04 15:52:50 -0400

    setup redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode> 
-    <App />
+    <App store={store}/>
   </StrictMode>
 );
 
