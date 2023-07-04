087c6c44e3cf5108a12ecee028be5aa79cbaf909
Ron <rc72095@icloud.com>
2023-07-04 18:04:48 -0400

    add logger and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = 
+  createStore(searchRobots, applyMiddleware(thunk, logger));
 
 const root = createRoot(document.getElementById('root'));
 root.render(
